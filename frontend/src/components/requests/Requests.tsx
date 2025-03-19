dc3431ccb73ab2db09019457c31ecc89b3502530
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-03-19 11:34:39 +0500

    Requests component refactoring

--- frontend/src/components/requests/Requests.tsx
+++ frontend/src/components/requests/Requests.tsx
@@ -1,11 +1,11 @@
 import { Request } from '@/entities/request';
-import styles from './Requests.module.scss';
-import classnames from 'classnames/bind';
 import { Pagination, Box, Typography, CircularProgress } from '@mui/material';
 import { RequestCard, Map } from '@/components';
 import { usePagination } from '@/hooks';
 import { Suspense, useEffect } from 'react';
 import NotFoundResult from '@/assets/images/not-found-result.svg?react';
+import styles from './Requests.module.scss';
+import classnames from 'classnames/bind';
 const cx = classnames.bind(styles);
 
 interface RequestsProps {
@@ -23,30 +23,34 @@ export function Requests({
   onRemoveRequestFromFavourites,
   onMakeDonationClick,
 }: RequestsProps) {
-  const [currentPageRequests, pagination, goToPage, goToPrevPage] =
-    usePagination(requests, 3);
+  const [currentPageRequests, pagination, goToPage] = usePagination(
+    requests,
+    3
+  );
 
   useEffect(() => {
     goToPage(1);
   }, []);
 
-  useEffect(() => {
-    if (!currentPageRequests.length) {
-      goToPrevPage();
-    }
-  }, [currentPageRequests]);
-
   const isMapActive = import.meta.env.VITE_MAPS_ENABLED && layout === 'map';
-  const renderRequests = () => {
-    if (requests.length && isMapActive) {
-      return (
+
+  if (!requests.length) {
+    return (
+      <Box className={cx(['requests-wrapper', 'requests-wrapper--no-result'])}>
+        <NotFoundResult />
+        <Typography variant="h5">Запросы не найдены</Typography>
+      </Box>
+    );
+  }
+
+  return (
+    <Box className={cx('requests-wrapper')}>
+      {isMapActive ? (
         <Suspense fallback={<CircularProgress />}>
           <Map requests={requests} />;
         </Suspense>
-      );
-    } else if (requests.length && !isMapActive) {
-      return (
-        <div className={cx(['requests', `requests--${layout}`])}>
+      ) : (
+        <Box className={cx(['requests', `requests--${layout}`])}>
           {currentPageRequests.map((request) => (
             <RequestCard
               key={request.id}
@@ -57,33 +61,11 @@ export function Requests({
               onMakeDonationClick={onMakeDonationClick}
             />
           ))}
-        </div>
-      );
-    } else {
-      return (
-        <Box
-          sx={{
-            height: '800px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <NotFoundResult />
-          <Typography variant="h5" sx={{ mt: '20px' }}>
-            Запросы не найдены
-          </Typography>
         </Box>
-      );
-    }
-  };
+      )}
 
-  return (
-    <div className={cx('requests-wrapper')} style={{ position: 'relative' }}>
-      {renderRequests()}
-      <Box className={cx('requests-pagination')}>
-        {requests.length && !isMapActive ? (
+      {requests.length && !isMapActive ? (
+        <Box className={cx('requests-pagination')}>
           <Pagination
             color="primary"
             size="large"
@@ -91,10 +73,9 @@ export function Requests({
             page={pagination.page}
             count={pagination.pagesCount}
             onChange={(_, page) => goToPage(page)}
-            sx={{ mb: '28px' }}
           />
-        ) : null}
-      </Box>
-    </div>
+        </Box>
+      ) : null}
+    </Box>
   );
 }
